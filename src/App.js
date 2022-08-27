a8f964cb6eff02c397e4832b64a97c85f0c3008c
denis <denis@kaizen-design.ru>
2022-08-27 17:20:21 +0400

    Add GeoJSON Location Data to a Map Using Markers and Popups in React Leaflet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
+const magicKingdomLatLng = [28.3852, -81.5639];
+
+function MagicKingdom() {
+  const map = useMap();
+  setTimeout(() => {
+    map.flyTo(magicKingdomLatLng, 14, {
+      duration: 3
+    })
+  }, 1000);  
+}
 
 function App() {
+  const location = [39.50, -98.35];
+  const zoom = 4;  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={location} zoom={zoom} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />     
+        <MagicKingdom />   
+        <Marker position={location}>
+          <Popup>
+            This is a popup example.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
